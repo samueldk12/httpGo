e3b0deb87d4962bdb16f1de10a325776ed00d653
Samuel Arao <samuel.arao@gmail.com>
2025-07-03 10:47:03 -0300

    modulo1: Aulas requests com contexto para configurações

--- m.go
+++ m.go
@@ -1,25 +1,37 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"io"
 	"net/http"
+	"time"
 )
 
 func main() {
-	resp, err := http.Post("https://ondetrabalho.com.br/api/v1/status", "application/json", nil)
+	ctx := context.Background()
+
+	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+
+	defer cancel()
+	
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, "http://ondetrabalho.com.br", nil)
 	
 	if err != nil {
 		panic(err)
 	}
 
-	defer resp.Body.Close()
+	resp, err := http.DefaultClient.Do(req)
 
-	data, err := io.ReadAll(resp.Body)
-
-	if err != nil {
+	if err != nil{
 		panic(err)
 	}
 
+	data, err := io.ReadAll(resp.Body)
+	
+	if err != nil{
+		panic(err)
+	}
+	
 	fmt.Println(string(data))
 }
